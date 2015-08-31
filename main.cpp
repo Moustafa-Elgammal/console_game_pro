9bd9642eda12e9a60d4335c499dff48f3f0394b0
Moustafa Mohammed Moustafa Elgammal <moustafa_algammal@yahoo.com>
2015-08-31 05:15:09 +0200

    add the main class "point" which will be used to handel the console area in coord X Y

--- main.cpp
+++ main.cpp
@@ -5,3 +5,48 @@
 #include <conio.h>
 #include <string>
 using namespace std;
+/*
+ *@class point
+ *which use to handle the console area
+ *in X , Y
+ */
+class point
+{
+	int x, y;
+public:
+    point(int u=0, int v=0) // u&v initializing values when creating new object without setting constructor parameters
+	{
+		set_point(u,v); //the
+	}
+
+    /** this function to set the x and y of the point position
+      * @para int u >> X axis
+      * @para int v >> Y axis
+      */
+	void set_point(int u, int v)
+	{
+		x = u;
+		y = v;
+	}
+
+    //system function to set position with co-ordinates
+	void gotoxy()
+	{//function definition
+		HANDLE hConsoleOutput;
+		COORD dwCursorPosition;
+		cout.flush();
+		dwCursorPosition.X = x;
+		dwCursorPosition.Y = y;
+		hConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+		SetConsoleCursorPosition(hConsoleOutput, dwCursorPosition);
+	}
+
+    //set operator
+	int operator <= (point p){ return (x <= p.x && y <= p.y); }
+	int operator >= (point p){ return (x >= p.x && y >= p.y); }
+	point operator += (point p){ return point(x += p.x, y += p.y); }
+};
+
+int main(int argc, char const *argv[]) {
+  return 0;
+}
