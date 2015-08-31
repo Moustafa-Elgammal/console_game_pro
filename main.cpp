dc2490a268fed64ad6d41da3a9aa1390371e18e9
Moustafa Mohammed Moustafa Elgammal <moustafa_algammal@yahoo.com>
2015-08-31 13:57:13 +0200

    adding the destroyer class and edit some parameters and the function

--- main.cpp
+++ main.cpp
@@ -56,15 +56,15 @@ public:
 class frame
 {
     // frame variables
-    int width;
-    int height;
-    int zero;
+    int frame_width;
+    int frame_height;
+    int frame_zero;
 public:
     frame ()
     {
-        width = global_width ;
-        height = global_height;
-        zero = global_zero;
+        frame_width = global_width ;
+        frame_height = global_height;
+        frame_zero = global_zero;
     }
 /**
  * This function to draw the frame game using class point
@@ -75,7 +75,7 @@ void draw_frame()
     point x;    //new object from point class  NOTE without constructor parameter because it's set default.
     //set the top line of the frame
     //              Start               END
-	for (x.set_point(zero, zero); x <= point(width, zero); x += point(1, 0))
+	for (x.set_point(frame_zero, frame_zero); x <= point(frame_width, frame_zero); x += point(1, 0))
 	{
 		x.gotoxy();
 		puts("*");
@@ -83,21 +83,21 @@ void draw_frame()
 
     //set the Right line of the frame
     //              Start               END
-	for (x.set_point(width, zero); x <= point(width, height); x += point(0, 1))
+	for (x.set_point(frame_width, frame_zero); x <= point(frame_width, frame_height); x += point(0, 1))
 	{
 		x.gotoxy();
         puts("*");
 	}
     //set the bottom line of the frame
     //              Start               END
-	for (x.set_point(zero,height); x <= point(width, height); x += point(1, 0))
+	for (x.set_point(frame_zero,frame_height); x <= point(frame_width, frame_height); x += point(1, 0))
 	{
 		x.gotoxy();
 		puts("*");
 	}
     //set the Left line of the frame
     //              Start               END
-	for (x.set_point(zero,zero); x <= point(zero, height); x += point(0, 1))
+	for (x.set_point(frame_zero,frame_zero); x <= point(frame_zero, frame_height); x += point(0, 1))
 	{
 		x.gotoxy();
 		puts("*");
@@ -106,7 +106,86 @@ void draw_frame()
 	cout<<"      |'d' move right||'r' reload blocks|"<<endl;
 	cout<<"      |'q' Quite"<<endl;
 }
+};
 
+class destroyer{
+    //destroyer properties
+    int destroyer_width;
+    int destroyer_bodyLeft;
+    int destroyer_bodyTop;
+    int destroyer_zero;
+public:
+    destroyer(){
+        destroyer_bodyLeft = global_width / 2;
+        destroyer_bodyTop = global_height - 1;
+        destroyer_width = global_width;
+        destroyer_zero = global_zero;
+    }
+    point x; // public object for the coordinates  X  Y
+
+    /**@function void draw_destroyer
+      *this function use to set the position of the
+      *destroyer body '^' then put it
+      */
+    void draw_destroyer()
+    {
+        x.set_point( destroyer_bodyLeft , destroyer_bodyTop );
+        x.gotoxy();
+        puts("^");
+    }
+
+    /**@function void delete_destroyer
+      *the function which use to clear the old position of the destroyer body
+      *before go to the new one
+      */
+    void delete_destroyer()
+    {
+        x.set_point( destroyer_bodyLeft, destroyer_bodyLeft);
+        x.gotoxy();
+        puts(" ");
+    }
+
+    /**
+      *this function to move the body to right
+      *at the new position setting
+      */
+    void move_destroyer_right()
+    {
+        if(destroyer_left < destroyer_width-1)
+        {
+            delete_destroyer();
+            x.set_point(bodyLeft+1, bodyTop);
+            x.gotoxy();
+            puts("i");
+            bodyLeft=bodyLeft+1;
+        }
+    }
+
+    /**
+     *this function to move the body to left
+     *at the new position setting
+     */
+    void moveLeft()
+    {
+        if(bodyLeft > zero+1)
+        {
+            deleteBody();
+            x.set_point(bodyLeft-1, bodyTop);
+            x.gotoxy();
+            puts("i");
+            bodyLeft=bodyLeft-1;
+        }
+    }
+
+    void killElement()
+    {
+        for(int i=2;i<2+3;i++)
+        {
+            x.set_point(bodyLeft, i);
+            x.gotoxy();
+            puts(" ");
+        }
+    }
 };
 
 int main(int argc, char const *argv[]) {
